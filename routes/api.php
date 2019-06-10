ef030ebb447aa9be3fca64755acdfa5af0430ce7
unknown <minhnb.it@gmail.com>
2019-06-10 21:11:40 +0700

    remove authen api

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
 
 Route::post('auth/jwt/register', 'Admin\UserController@register');
 Route::post('auth/jwt/login', 'Admin\UserController@login');
